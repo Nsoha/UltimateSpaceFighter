753e2855e5247b687bd2aa459c21a99066f9e080
Nsoha <nicholas.soha@maine.edu>
2016-11-24 01:37:40 -0500

    ok t

--- app/src/main/java/com/nick/ultimatespacefighter/doublePlayer.java
+++ app/src/main/java/com/nick/ultimatespacefighter/doublePlayer.java
@@ -1,19 +1,29 @@
 package com.nick.ultimatespacefighter;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class doublePlayer extends AppCompatActivity {
-
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_double_player);
+
     }
 
 
 
+public void scan(){
+    if (!mBluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+       // startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
 
+
+    }
 
 
 
